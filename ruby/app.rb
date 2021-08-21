09793c98e504cc261493df6c4caf00eba8f17a18
Hoang Minh Quang <hoang.minh.quang@classi.jp>
2021-08-21 14:22:13 +0900

    remove n+1 insert

--- ruby/app.rb
+++ ruby/app.rb
@@ -676,18 +676,24 @@ module Isucondition
         count = db.xquery('SELECT COUNT(*) AS `cnt` FROM `isu` WHERE `jia_isu_uuid` = ?', jia_isu_uuid).first
         halt_error 404, 'not found: isu' if count.fetch(:cnt).zero?
 
+        rows = []
+        json_params.each do |cond|
+          timestamp = Time.at(cond.fetch(:timestamp))
+          rows << "('%s', %s, %s, '%s', '%s')"  % [ jia_isu_uuid, timestamp, cond.fetch(:is_sitting), cond.fetch(:condition), cond.fetch(:message)]
+        end
         json_params.each do |cond|
           timestamp = Time.at(cond.fetch(:timestamp))
           halt_error 400, 'bad request body' unless valid_condition_format?(cond.fetch(:condition))
 
-          db.xquery(
-            'INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES (?, ?, ?, ?, ?)',
-            jia_isu_uuid,
-            timestamp,
-            cond.fetch(:is_sitting),
-            cond.fetch(:condition),
-            cond.fetch(:message),
-          )
+          # db.xquery(
+          #   'INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES (?, ?, ?, ?, ?)',
+          #   jia_isu_uuid,
+          #   timestamp,
+          #   cond.fetch(:is_sitting),
+          #   cond.fetch(:condition),
+          #   cond.fetch(:message),
+          # )
+          db.xquery("INSERT INTO `isu_condition` (`jia_isu_uuid`, `timestamp`, `is_sitting`, `condition`, `message`) VALUES #{rows.join(',')}")
         end
       end
 
